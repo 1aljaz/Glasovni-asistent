eb38163f2fa1323ecf8bca1f58ee8a32073016b5
Aljaz Marn <azo.marn@gmail.com>
2022-03-27 21:59:27 +0200

    Removed Vosk, added Speech recognition and a Generic Assistant
    Vosk was replaced by a speech recognition by google. I also partially implemented a generic assistant.

--- main.py
+++ main.py
@@ -1,20 +1,75 @@
-from vosk import Model, KaldiRecognizer
-import os
-import pyaudio
+from email import message
+import speech_recognition as sr
+from neuralintents import GenericAssistant as ga
+import pyttsx3 as tts
+import sys
 
-model = Model("model")
-rec = KaldiRecognizer(model, 16000)
+r = sr.Recognizer()
 
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, frames_per_buffer=8000)
-stream.start_stream()
+speaker = tts.init()
+speaker.setProperty('rate', 150)
+
+todo_list = {'Add note'}
+
+def create_note():
+    global recognizer
+
+    speaker.say("What do you want me to add to your notes")
+    speaker.runAndWait()
+
+    done = False
+
+    while not done:
+
+        try:
+
+            with sr.Microphone() as mic:
+
+                r.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = r.listen(mic)
+
+                note = r.recognize_google(audio)
+
+                note = note.lower()
+
+                speaker.say ("Choose a filename")
+                speaker.runAndWait()
+
+                r.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = r.listen(mic)
+
+                filename = r.recognize_google(audio)
+                filename = filename.lower()
+
+            with open(filename, 'w') as f:
+            
+                f.write(note)
+                speaker.say ("I made a note {filename}")
+                speaker.runAndWait
+
+        except sr.UnknownValueError:
+            r = sr.Recognizer()
+            speaker.say("Didnt recognzie, please try again")
+            speaker.runAndWait()
+
+mappings = {'note': create_note}
+
+assistant = ga('intents.json', intent_methods=mappings)
+assistant.train_model()
 
 while True:
-    data = stream.read(4000)
-    if (len(data) == 0): 
-        break
-    if (rec.AcceptWaveform(data)):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
-print (rec.FInalResult())
+
+    try:
+        with sr.Microphone() as mic:
+
+            r.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = r.listen(mic)
+
+            message = r.recognize_google(audio)
+            message = message.lower()
+
+        assistant.request(message)
+
+    except sr.UnknownValueError:
+        r = sr.Recognizer()
+
