f40658ed21e5f75e4a3d67e5fdccbcb7a02eef78
Aljaz Marn <azo.marn@gmail.com>
2022-03-28 01:19:28 +0200

    Added some more functions to an assistant
    Added the following functions:
    - create todo
    - show todo
    -quit
    -hello

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from email import message
+from neuralintents import GenericAssistant
 import speech_recognition as sr
-from neuralintents import GenericAssistant as ga
 import pyttsx3 as tts
 import sys
 
@@ -9,30 +8,26 @@ r = sr.Recognizer()
 speaker = tts.init()
 speaker.setProperty('rate', 150)
 
-todo_list = {'Add note'}
+todo_list = ['Clean my room', 'Study']
 
-def create_note():
-    global recognizer
+def create_notes():
+    global r
 
-    speaker.say("What do you want me to add to your notes")
+    speaker.say("What to you want to write onto your note")
     speaker.runAndWait()
 
     done = False
 
     while not done:
-
         try:
-
             with sr.Microphone() as mic:
-
                 r.adjust_for_ambient_noise(mic, duration=0.2)
                 audio = r.listen(mic)
 
                 note = r.recognize_google(audio)
-
                 note = note.lower()
 
-                speaker.say ("Choose a filename")
+                speaker.say("Choose a filename")
                 speaker.runAndWait()
 
                 r.adjust_for_ambient_noise(mic, duration=0.2)
@@ -42,34 +37,82 @@ def create_note():
                 filename = filename.lower()
 
             with open(filename, 'w') as f:
-            
                 f.write(note)
-                speaker.say ("I made a note {filename}")
-                speaker.runAndWait
-
+                done = True
+                speaker.say("I succesfully created a file with a name {filename}", filename)
+                speaker.runAndWait()
         except sr.UnknownValueError:
             r = sr.Recognizer()
-            speaker.say("Didnt recognzie, please try again")
+            speaker.say("Didnt quite catch that, please repeat")
+
+def create_todo():
+    global r
+
+    speaker.say("What woudl you like to add to your todo list")
+    speaker.runAndWait()
+
+    done = False
+
+    while not done:
+        try:
+            with sr.Microphone() as mic:
+                r.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = r.listen(mic)
+
+                todo = r.recognize_google(audio)
+                todo = todo.lower()
+
+                todo_list.append(todo)
+                done = True
+
+                speaker.say("I added {todo} to your todo list")
+                speaker.runAndWait()
+        except sr.UnknownValueError:
+            r = sr.Recognizer()
+            speaker.say("I didnt understand, please repeat")
             speaker.runAndWait()
 
-mappings = {'note': create_note}
+def show_todo():
+    global r
 
-assistant = ga('intents.json', intent_methods=mappings)
+    speaker.say("Your todos are")
+    for item in todo_list:
+        speaker.say(item)
+    speaker.runAndWait()
+
+def hello():
+    speaker.say("Hello, what can I do for you")
+    speaker.runAndWait()
+
+def quit():
+    speaker.say("It was nice chatting with you")
+    speaker.runAndWait()
+    exit(1)
+
+
+mappings = {
+    "create_notes": create_notes,
+    "create_todos": create_todo,
+    "show_todos":show_todo,
+    "greeting": hello,
+    "goodbye": quit,
+
+}
+
+assistant = GenericAssistant('intents.json', intent_methods=mappings)
 assistant.train_model()
 
 while True:
-
     try:
         with sr.Microphone() as mic:
-
             r.adjust_for_ambient_noise(mic, duration=0.2)
             audio = r.listen(mic)
 
             message = r.recognize_google(audio)
             message = message.lower()
 
-        assistant.request(message)
+            print(message)
 
+            assistant.request(message)
     except sr.UnknownValueError:
         r = sr.Recognizer()
-
