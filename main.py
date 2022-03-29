7aca698ccd4449941ff5ea0f2b1421de725e5bbb
Aljaz Marn <azo.marn@gmail.com>
2022-03-29 23:22:37 +0200

    Optimized code
    A grouped some functions and made code faster and easier to read

--- main.py
+++ main.py
@@ -1,140 +1,26 @@
 from datetime import datetime
-from cv2 import exp
 from neuralintents import GenericAssistant
 import speech_recognition as sr
 import pyttsx3 as tts
-import sys
 import wikipedia as wiki
 
 r = sr.Recognizer()
 
-speaker = tts.init('sapi5')
-voices=speaker.getProperty('voices')
-speaker.setProperty('voice','voices[0].id')
+engine = tts.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
 
-todo_list = ['Clean my room', 'Study']
+todo_list = {"Clean my room", "Make science project"}
 
-def create_notes():
+def talk(command):
+    engine.say(command)
+    engine.runAndWait()
+
+def listen():
     global r
-
-    speak("What to you want to write onto your note")
 
     done = False
-
-    while not done:
-        try:
-            with sr.Microphone() as mic:
-                r.adjust_for_ambient_noise(mic, duration=0.2)
-                audio = r.listen(mic)
-
-                note = r.recognize_google(audio)
-                note = note.lower()
-
-                speak("Choose a filename")
-
-                r.adjust_for_ambient_noise(mic, duration=0.2)
-                audio = r.listen(mic)
-
-                filename = r.recognize_google(audio)
-                filename = filename.lower()
-
-            with open(filename, 'w') as f:
-                f.write(note)
-                done = True
-                speak(f"I succesfully created a file with a name {filename}")
-
-        except sr.UnknownValueError:
-            r = sr.Recognizer()
-            speak("Didnt quite catch that, please repeat")
-
-def wishMe():
-    hour = datetime.now().hour
-
-    if hour >= 0 and hour <12:
-        speak("Good Morning")
-    elif hour >=12 and hour > 18:
-        speak("Good afternoon")
-    else:
-        speak("Good Evening")
-
-def wikipeida():
-    global r
-    speak("What would like me to search for you")
-
-    done = False
-
-    while not done:
-        try:
-            with sr.Microphone() as mic:
-                r.adjust_for_ambient_noise(mic, duration=0.2)
-                audio = r.listen(mic)
-
-                query = r.recognize_google(audio)
-                query = query.lower()
-
-                print (query)
-
-                if query == 'exit':
-                    exit(1)
-
-                result = wiki.summary(query, sentences=3)
-
-                speak(f"You have searched for {query}")
-                speak(result)
-        except sr.UnknownValueError:
-            r = sr.Recognizer()
-            speak("I didnt understand, please repeat")
-        except wiki.exceptions.PageError:
-            speak("The page doesnt exist")
-        except wiki.exceptions.DisambiguationError:
-            speak("There are too many result, try again")
-            continue
-
-
-def create_todo():
-    global r
-
-    speak("What woudl you like to add to your todo list")
-
-    done = False
-
-    while not done:
-        try:
-            with sr.Microphone() as mic:
-                r.adjust_for_ambient_noise(mic, duration=0.2)
-                audio = r.listen(mic)
-
-                todo = r.recognize_google(audio)
-                todo = todo.lower()
-                
-                if todo == 'abort':
-                    break
-
-                todo_list.append(todo)
-                done = True
-                
-                speak(f"I added {todo} to your todo list")
-
-        except sr.UnknownValueError:
-            r = sr.Recognizer()
-            speak("I didnt understand, please repeat")
-           
-
-def show_todo():
-    global r
-
-    speaker.say("Your todos are")
-    for item in todo_list:
-        speak(item)
-
-def quit():
-    global r
-    
-    speak("Please confirm shutting me down, by saying the super secret password")
-    # password is: niger
-    done = False
-
-    while not done:
+    while not done:   
         try:
             with sr.Microphone() as mic:
                 r.adjust_for_ambient_noise(mic, duration=0.2)
@@ -142,50 +28,86 @@ def quit():
 
                 command = r.recognize_google(audio)
                 command = command.lower()
+                if 'alexa' in command:
+                    command = command.replace('alexa ', '')
+                    print (command)
+                return command
 
-                print(command)
-
-                if command == 'niger':
-                    speak("It was nice chatting with you")
-                    exit(1)
-                elif command != 'niger':
-                    speak ("You didnt confirm, aborting shutdown")
-                    break
         except sr.UnknownValueError:
             r = sr.Recognizer()
-            speak("Please repeat")
+       
 
-def thanks():
-    speak("You are welcome")
+def greeting():
+    hour = datetime.now().hour
 
-def speak(text):
-    speaker.say(text)
-    speaker.runAndWait()
+    if hour >= 0 and hour <12:
+        talk("Good Morning")
+    elif hour >=12 and hour > 18:
+        talk("Good afternoon")
+    else:
+        talk("Good Evening")
+
+def wikipedia():
+
+    talk("What do you want me to search")
+
+    query = listen()
+    
+    print (query)
+    if 'quit' in query:
+        talk ("Quitting")
+        return
+
+    try:        
+        talk (f"According to wikipedia {query} is")
+        result = wiki.summary(query, sentences=3)
+    except wiki.exceptions.PageError:
+        talk("Page doesnt exist")
+    except wiki.exceptions.DisambiguationError:
+        talk("There are too many results, try again")
+    talk(result)
+
+def create_todo():
+
+    talk ("What would you like to add to your todo list")
+
+    todo = listen()
+
+    todo_list.append(todo)
+
+    talk (f"I added {todo} to your todos")
+    print (f"{todo}")
+
+def show_todo():
+    for item in todo_list:
+        talk (item)
+
+def create_note():
+    talk ("What would you like to write to your notes")
+    note = listen()
+
+    talk ("Choose a filename")
+    filename = listen()
+
+    with open(filename, 'w') as f:
+        f.write(note)
+    talk (f"Succesfully created a note with a name {filename}")
 
 mappings = {
-    "create_notes": create_notes,
-    "add_todo": create_todo,
-    "show_todos":show_todo,
-    "greeting": wishMe,
-    "goodbye": quit,
-    "thanks": thanks,
-    "wikipedia": wikipeida
+    "wikipedia": wikipedia,
+    "create_todo": create_todo,
+    "show_todos": show_todo,
+    "create_notes": create_note
 }
 
-assistant = GenericAssistant('intents.json', intent_methods=mappings)
+assistant = GenericAssistant('intents_better.json', intent_methods=mappings)
 assistant.train_model()
 
+greeting()
+
 while True:
-    try:
-        with sr.Microphone() as mic:
-            r.adjust_for_ambient_noise(mic, duration=0.2)
-            audio = r.listen(mic)
+    message = listen()
+    print(message)
 
-            message = r.recognize_google(audio)
-            message = message.lower()
+    assistant.request(message)
 
-            print(message)
-
-            assistant.request(message)
-    except sr.UnknownValueError:
-        r = sr.Recognizer()
